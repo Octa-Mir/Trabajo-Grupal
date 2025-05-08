50bae44f1c608f5467e0cc6abf9504aa76fb2d0b
mau.uze <mauriciouzedarocha@gmail.com>
2025-05-08 01:27:20 -0400

    implementación del metodo resta en la calculadora

--- calculadora.py
+++ calculadora.py
@@ -2,6 +2,9 @@
 def suma(a, b):
     return a + b
 
+def resta(a, b):
+    return a - b
+
 def calculadora():
     print("Calculadora simple")
     print("Operaciones disponibles: +")
@@ -12,8 +15,13 @@ def calculadora():
 
     if operacion == '+':
         resultado = suma(a, b)
+
+    elif operacion == '-':
+        resultado = resta(a, b)
+        
     else:
         resultado = "Operación no válida"
 
     print("Resultado:", resultado)
+
 calculadora()
